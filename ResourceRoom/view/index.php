c571e9e4c3e65659d2795955c4986eea3a775595
Brady <Brady@DESKTOP-V3D7B82>
2021-09-12 14:39:07 -0700

    Updating the nav bar

--- ResourceRoom/view/index.php
+++ ResourceRoom/view/index.php
@@ -2,10 +2,10 @@
     $title = "Home Page";
     require '../view/headerInclude.php';
 ?>
-    <section id="main">
+    <section id="main" class="clarion-blue">
         <div class="container-fluid">
             <div class ="row">
-                <div class="col-2">
+                <div class="col-2 clarion-white">
                     <h3>Catagory Box</h3>
                 </div>
                 <div class="col-10">
