42255dcf2dd5141670035b6ae7697abbdcd3e206
BBizzarri <bradybizzarri1@gmail.com>
2021-09-14 19:30:21 -0400

    Hope this works

--- ResourceRoom/view/navInclude.php
+++ ResourceRoom/view/navInclude.php
@@ -1,5 +1,5 @@
 
-    <nav class="navbar navbar-expand-md navbar-light sticky-top">
+    <nav class="navbar navbar-expand-md clarion-gold navbar-light sticky-top">
         <a class="navbar-brand d-md-none d-lg-block" href="../controller/controller.php?action=Home">Resource Room</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
