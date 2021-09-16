66a18f1e9671b297406122aeae0e882a871f5998
BBizzarri <bradybizzarri1@gmail.com>
2021-09-16 17:12:46 -0400

    Finished UI For admin inventory page

--- ResourceRoom/controller/shopperCasesInclude.php
+++ ResourceRoom/controller/shopperCasesInclude.php
@@ -2,15 +2,15 @@
 
     // This file is included in the main controller as a series of cases to check the $action querystring parameter.
     // The purpose is to separate the shopper actions from the back-end inventory actions to help version control.
-//     switch ($action) {
-//         case 'shopperCart':
-//             include '../view/shopperCart.php';
-//             break;
-//         case 'shopperHome':
-//             include '../view/index.php';
-//             break;
-//         case 'shopperOrders':
-//             include '../view/shopperOrders.php';
-//             break;
-//     }
+    switch ($action) {
+        case 'shopperCart':
+            include '../view/shopperCart.php';
+            break;
+        case 'shopperHome':
+            include '../view/index.php';
+            break;
+        case 'shopperOrders':
+            include '../view/shopperOrders.php';
+            break;
+    }
 ?>
--- ResourceRoom/view/navInclude.php
+++ ResourceRoom/view/navInclude.php
@@ -1,33 +1,96 @@
+ <?php 
+        if(!loggedIn()) {
+    ?>        
+        <nav class="clarion-gold navbar navbar-expand-md sticky-top">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <nav class="navbar navbar-expand-md clarion-gold navbar-light sticky-top">
-        <a class="navbar-brand d-md-none d-lg-block" href="../controller/controller.php?action=Home">Resource Room</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=shopperHome">Resource Room</a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=shopperCart">Cart (0)</a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=shopperOrders">Orders</a>
+                        </li>
+                        <li class="nav-item">
+                            <?php
+                                if (!loggedIn()) {
+                                    echo "<a class='nav-link' href='../security/index.php?action=SecurityLogin&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'><i class='fas fa-sign-in-alt'></i> Log In </a>";
+                                }else {
+                                    echo "<a class='nav-link' href='../security/index.php?action=SecurityLogOut&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'>Log Out " . $_SESSION['UserName'] . " </a>";
+                                }
+                            ?>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="../controller/controller.php?action=Home">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../security/index.php">Security <span class="sr-only"></span></a>
-                </li>
-                <li class="nav-item">
-                    <?php
-                        if (!loggedIn()) {
-                            echo "<a class='nav-link' href='../security/index.php?action=SecurityLogin&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'><i class='fas fa-sign-in-alt'></i> Log In </a>";
-                        }else {
-                            echo "<a class='nav-link' href='../security/index.php?action=SecurityLogOut&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'>Log Out " . $_SESSION['UserName'] . " </a>";
-                        }
-                    ?>
+                        </li>
+                    </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            <div class="dropdown account-dropdown">
+                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown"><img src="../Images/person-icon.png" alt="person" height="40px" width="40px"/></button>
+              <ul class="dropdown-menu">
+                <li><a style="color: black;" href="#">Account Settings</a></li>
+                <li><a style="color: black" href="#">Logout</a></li>
+              </ul>
+            </div>
+            </div>
+        </nav>
+    <?php
+        }
+        else {
+    ?>
+        <nav class="clarion-gold navbar navbar-expand-md sticky-top">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=adminOrders">Orders (0)<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=adminInventory">Inventory<span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=adminReports">Reports<span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=adminShoppingList">Shopping List<span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="color: white; font-size: 20px;" class="nav-link admin-user-nav-bar-text" href="../controller/controller.php?action=adminSecurity">Security<span class="sr-only"></span></a>
+                        </li>
+                        <li class="nav-item">
+                            <?php
+                                if (!loggedIn()) {
+                                    echo "<a class='nav-link' href='../security/index.php?action=SecurityLogin&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'><i class='fas fa-sign-in-alt'></i> Log In </a>";
+                                }else {
+                                    echo "<a class='nav-link' href='../security/index.php?action=SecurityLogOut&RequestedPage=" . urlencode($_SERVER['REQUEST_URI'])  .  "'>Log Out " . $_SESSION['UserName'] . " </a>";
+                                }
+                            ?>
 
+                        </li>
+                    </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            <div class="dropdown account-dropdown">
+                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown"><img src="../Images/person-icon.png" alt="person" height="40px" width="40px"/></button>
+              <ul class="dropdown-menu">
+                <li><a style="color: black;" href="#">Account Settings</a></li>
+                <li><a style="color: black" href="#">Logout</a></li>
+              </ul>
+            </div>
+            </div>
+        </nav>
+    <?php
+        }
+    ?>
