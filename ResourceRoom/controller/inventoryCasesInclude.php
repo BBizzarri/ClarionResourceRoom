ffd3f63423ca7636de144475f9d5184988f9d401
BBizzarri <bradybizzarri1@gmail.com>
2021-09-17 12:41:22 -0400

    Finished UI For admin inventory page

--- ResourceRoom/controller/inventoryCasesInclude.php
+++ ResourceRoom/controller/inventoryCasesInclude.php
@@ -3,22 +3,22 @@
     // This file is included in the main controller as a series of cases to check the $action querystring parameter.
     // The purpose is to separate the shopper actions from the back-end inventory actions to help version control.
 
-//     switch ($action) {
-//         case 'adminInventory':
-//             include '../view/adminInventory.php';
-//             break;
-//         case 'adminOrders':
-//             include '../view/adminOrders.php';
-//             break;
-//         case 'adminReports':
-//             include '../view/adminReports.php';
-//             break;
-//         case 'adminSecurity':
-//             include '../security/index.php';
-//             break;
-//         case 'adminShoppingList':
-//             include '../view/adminShoppingList.php';
-//             break;
-//     }
+    switch ($action) {
+        case 'adminInventory':
+            include '../view/adminInventory.php';
+            break;
+        case 'adminOrders':
+            include '../view/adminOrders.php';
+            break;
+        case 'adminReports':
+            include '../view/adminReports.php';
+            break;
+        case 'adminSecurity':
+            include '../security/index.php';
+            break;
+        case 'adminShoppingList':
+            include '../view/adminShoppingList.php';
+            break;
+    }
 
 ?>
