aa1114950735d4bc6b723def4a67ad0412f743d3
ShaneW1 <s.m.wice@eagle.clarion.edu>
2021-09-29 14:04:09 -0400

    Merged branch with staging, card fixed, and search "working"

---
+++ ResourceRoom/model/product.php
@@ -0,0 +1,11 @@
+<?php
+class product
+{
+    //Properties
+    public $ProductID;
+    public $Name;
+    public $Description;
+    public $QtyOnHand;
+    public $MaxOrderQty;
+    public $GoalStock;
+}
