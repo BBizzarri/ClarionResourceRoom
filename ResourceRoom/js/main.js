4845fad716181ca0de29ac14aae717967576c1b8
BBizzarri <bradybizzarri1@gmail.com>
2021-09-30 09:17:39 -0400

    Fixing css issues from merge

--- ResourceRoom/js/main.js
+++ ResourceRoom/js/main.js
@@ -1,7 +1,7 @@
 function generalSearch(){
     var criteria = '';
     criteria = $(`#Criteria`).val();
-    alert(criteria)
+    alert(criteria);
     document.location="../controller/controller.php?action=shopperHome&ListType=GeneralSearch&Criteria=" + encodeURIComponent(criteria);
 }
 
--- ResourceRoom/view/headerInclude.php
+++ ResourceRoom/view/headerInclude.php
@@ -10,10 +10,10 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/main.css" >
     <link rel="stylesheet" href="../css/admin.css" >
-    <script src="../js/main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
 
 </head>
 <body>
