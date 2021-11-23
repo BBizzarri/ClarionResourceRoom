51a79f05e05bd21887417a2400f9629bb0e9bb33
ShaneW1 <s.m.wice@eagle.clarion.edu>
2021-11-23 17:58:20 -0500

    Admin inventory search removed

--- ResourceRoom/view/adminInventory.php
+++ ResourceRoom/view/adminInventory.php
@@ -223,6 +223,8 @@
                                       {
                                           $("#inventoryTable").DataTable(
                                               {
+                                                  searching: false,
+
                                                   dom:'Bfrtip',
                                                   buttons: [
                                                       {
